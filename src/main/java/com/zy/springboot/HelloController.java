88df69eabdcdd6a1f694938a51e869ba294fd1c1
zhang-yue-github <1043647269@qq.com>
2021-01-19 22:16:43 +0800

    commit all

--- src/main/java/com/zy/springboot/HelloController.java
+++ src/main/java/com/zy/springboot/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Spring Boot!！";
+        return "Hello Spring Boot!！!";
     }
 
 }
