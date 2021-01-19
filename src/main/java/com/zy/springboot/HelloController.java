029385a3736f3c59fc743f61c9614eff5d4695b4
zhang-yue-github <1043647269@qq.com>
2021-01-19 22:15:34 +0800

    commit all

---
+++ src/main/java/com/zy/springboot/HelloController.java
@@ -0,0 +1,19 @@
+package com.zy.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangyue
+ * @since 2020/1/4 3:45 下午
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!！";
+    }
+
+}
